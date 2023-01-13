e4cc080839128c06f71cd7131062ab71888e36e2
a6uxa4 <a6uxa4@icloud.com>
2023-01-13 23:08:10 +0600

    save

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import './App.css'
 function App() {
 	const [min, setMin] = useState(10)
 	return (
-		<div>
-			<div className='Container'>
-				<span>{min}</span>
-			</div>
+		<div className='Container'>
+			<span>{min}</span>
 		</div>
 	)
 }
